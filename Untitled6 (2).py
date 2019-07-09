3cc1721b94c91a6243896c894b124d7e9f7ba6ca
221810401026 <52528697+221810401026@users.noreply.github.com>
2019-07-09 17:16:26 +0530

    Add files via upload

---
+++ Untitled6 (2).py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# 1) [1,4,5,6,9] - 14569
+def convert(s):
+    x=0
+    for i in range(len(s)):
+        x=x*10+s[i]
+    return x
+s=[1,4,5,6,9]
+convert(s)
+
+
+# In[4]:
+
+
+# 2) [1,4,5,6,9]  - 46 (Consider only even numbers from list)
+def even(s):
+    x=0
+    for i in range(len(s)):
+        if s[i]%2==0:
+            x=x*10+s[i]
+    return x
+s=[1,4,5,6,9]
+even(s)
+
+
+# In[5]:
+
+
+# 3. [1,2,3,4,5] - [1,4,3,16,5] (Consider only even number and square of it)
+def square(s):
+    for i in range (len(s)):
+        if s[i]%2==0:
+            s[i]=s[i]**2
+    return s
+s=[1,2,3,4,5]
+square(s)
+
